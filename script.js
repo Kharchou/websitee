d7745e607d61b5515762d7e71732bea92dd6eadf
Glitch (glitch-hello-website) <none>
2025-03-05 06:07:23 +0000

    💿🎥 Checkpoint ./index.html:67454668/3339 ./script.js:67454668/5367 ./style.css:67454668/6654

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
+const menuItems = document.querySelectorAll('.menu-item');
+const rightContent = document.querySelector('.right-content');
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
-
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
+const images = {
+    film: [
+        'image1.jpg',
+        'image2.jpg',
+        'image3.jpg'
+    ],
+    'video-installation': [
+        'video1.jpg',
+        'video2.jpg',
+        'video3.jpg'
+    ],
+    prints: [
+        'print1.jpg',
+        'print2.jpg',
+        'print3.jpg'
+    ]
 };
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+
+menuItems.forEach((menuItem) => {
+    menuItem.addEventListener('click', (e) => {
+        e.preventDefault();
+        const category = menuItem.getAttribute('data-category');
+        const imagesHtml = images[category].map((image) => {
+            return `<img src="${image}" alt="">`;
+        }).join('');
+        rightContent.innerHTML = imagesHtml;
+    });
 });
