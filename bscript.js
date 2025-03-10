519daa649896344391fee2e9d728cb378da3cfeb
Glitch (glitch-hello-website) <none>
2025-03-10 19:55:56 +0000

    🌜⛰ Checkpoint ./.glitch-assets:67454668/1174 ./bscript.js:67454668/1680 ./bstyle.css:67454668/2576 ./About.html:67454668/3007 ./Works.html:67454668/2862 ./style.css:67454668/1813 ./wstyle.css:67454668/3475

--- bscript.js
+++ bscript.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Array of image filenames
-    const images = ["bio1.jpg", "bio2.jpg", "bio3.jpg"];
+  // Array of image filenames
+  const images = [
+    "https://cdn.glitch.global/7e04c109-bed7-42b3-9fc9-ebf6a7bcda80/Kharchou_1.png?v=1741635902302",
+    "https://cdn.glitch.global/7e04c109-bed7-42b3-9fc9-ebf6a7bcda80/Kharchou_2.png?v=1741635911714",
+    "https://cdn.glitch.global/7e04c109-bed7-42b3-9fc9-ebf6a7bcda80/Kharchou_3.png?v=1741635917085",
+  ];
 
-    // Pick a random image
-    const randomImage = images[Math.floor(Math.random() * images.length)];
+  // Pick a random image
+  const randomImage = images[Math.floor(Math.random() * images.length)];
 
-    // Set the image source
-    document.getElementById("bio-image").src = randomImage;
+  // Set the image source
+  document.getElementById("bio-image").src = randomImage;
 });
